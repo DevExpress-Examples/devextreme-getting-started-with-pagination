e4962309ef13aabad33facb328b831c87cc6b043
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-05-23 09:51:58 +0300

    chore(angular): ng update 20 → 21

--- Angular/src/main.ts
+++ Angular/src/main.ts
@@ -1,8 +1,9 @@
+import { provideZoneChangeDetection } from "@angular/core";
 /* eslint-disable no-console */
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic().bootstrapModule(AppModule, { applicationProviders: [provideZoneChangeDetection()], })
   // @ts-expect-warning: `console.error` is used here intentionally for simple error logging during bootstrap
   .catch((err) => console.error(err));
