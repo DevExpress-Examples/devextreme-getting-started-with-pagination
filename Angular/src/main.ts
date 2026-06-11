318a4bff3b7a06792f3f06de35eb85478d947eea
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-06-11 21:45:55 +0300

    Fix DevExtreme licensing import order

--- Angular/src/main.ts
+++ Angular/src/main.ts
@@ -1,12 +1,11 @@
-import config from 'devextreme/core/config';
-import { licenseKey } from './devextreme-license';
-
-config({ licenseKey });
-
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideZoneChangeDetection } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
+import config from 'devextreme/core/config';
 import { AppComponent } from './app/app.component';
+import { licenseKey } from './devextreme-license';
+
+config({ licenseKey });
 
 bootstrapApplication(AppComponent, {
   providers: [provideZoneChangeDetection(), provideHttpClient()],
